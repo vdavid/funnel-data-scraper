9168cbde1221cf7c3d0db35318f20f7bd9f05c71
Veszelovszki Dávid <veszelovszki@gmail.com>
2017-09-08 12:29:31 +0200

    Converted output to CSV for easier use.

--- index.js
+++ index.js
@@ -31,13 +31,46 @@ async function run() {
 
     let userCounts = {};
     userCounts = await intercomHtmlPage.getSlackNumbers(userCounts, FIRST_DAY_OF_WEEK_TO_SCRAPE, 7);
-    userCounts = await intercomHtmlPage.getPaymentFunnelNumbers(userCounts, FIRST_DAY_OF_WEEK_TO_SCRAPE, 7);
     userCounts = await intercomHtmlPage.getSubmissionCountRelatedNumbers(userCounts, FIRST_DAY_OF_WEEK_TO_SCRAPE, 7);
-    logger.log(JSON.stringify(userCounts));
+    userCounts = await intercomHtmlPage.getPaymentFunnelNumbers(userCounts, FIRST_DAY_OF_WEEK_TO_SCRAPE, 7);
 
+    logger.log(JSON.stringify(userCounts));
+    logger.log(await convertOutputToCsv(userCounts));
     logger.log('Finished.');
 
     browser.close();
+}
+
+async function convertOutputToCsv(userCounts) {
+    const outputArray = [];
+    for (let metric in userCounts) {
+        if (userCounts.hasOwnProperty(metric)) {
+            let metricArray = [];
+            metricArray.push(metric);
+            for (let locale in userCounts[metric]) {
+                if (userCounts[metric].hasOwnProperty(locale)) {
+                    for (let utm in userCounts[metric][locale]) {
+                        if (userCounts[metric][locale].hasOwnProperty(utm)) {
+                            metricArray.push(userCounts[metric][locale][utm]);
+                        }
+                    }
+                    metricArray.push('');
+                }
+            }
+            outputArray.push(metricArray);
+        }
+    }
+    return matrixToCsv(transpose(outputArray));
+}
+
+function transpose(matrix) {
+    return matrix[0].map((column, columnIndex) => matrix.map((row, rowIndex) => matrix[rowIndex][columnIndex]))
+}
+
+function matrixToCsv(matrix) {
+    return matrix.map(function (row) {
+        return row.join('\t');
+    }).join('\n');
 }
 
 run();
